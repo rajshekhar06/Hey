e25fee6408273cfa4c84e8d5a288169a459a1fa4
rajshekhar <rajzzgs@gmail.com>
2024-03-10 21:18:04 +0530

    Code changes

---
+++ cal.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class cal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        System.out.println("Select operation:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+
+        scanner.close();
+    }
+}
